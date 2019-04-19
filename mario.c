cf179f2df4649075c777b9a8faacb36cb52614f0
Jackylyn1 <Jackylyn1@users.noreply.github.com>
2019-04-19 20:53:16 +0000

    20190419T205259Z

--- mario.c
+++ mario.c
@@ -17,9 +17,9 @@ int main(void)
 
 void bricks(int n)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        printSigns(7 - i,' ');
+        printSigns(n - i,' ');
         printSigns(i,'#');
         printf("\n");
     }
@@ -27,7 +27,7 @@ void bricks(int n)
 
 void printSigns(int i, char c)
 {
-     for (int j = 0; j <= i; j++)
+     for (int j = 0; j < i; j++)
       {
           printf("%c", c);
       }
