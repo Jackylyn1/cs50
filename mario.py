6d8bc1c4d2c8b78ed5467c679a6eff9b7e4cd41c
Jackylyn1 <Jackylyn1@users.noreply.github.com>
2019-04-27 09:47:22 +0000

    20190427T094622Z

--- mario.py
+++ mario.py
@@ -3,9 +3,6 @@ from cs50 import get_int
 height = get_int("Height: ")
 
 for n in range(height):
-    for i in range(height):
-        if i < (height - n - 1):
-            print("-", end = "")
-        else:
-            print("#", end = "")
+    print(" " * (height - n - 1), end = "")
+    print("#" * (n + 1), end = "")
     print()
