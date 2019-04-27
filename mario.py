65a0c0aa134e473e8a444ba3adbe9f4665a7f7bd
Jackylyn1 <Jackylyn1@users.noreply.github.com>
2019-04-27 10:04:43 +0000

    20190427T100343Z

--- mario.py
+++ mario.py
@@ -1,8 +1,11 @@
 from cs50 import get_int
 
-height = get_int("Height: ")
+while True:
+    height = get_int("Height: ")
+    if height > 0 and height < 9:
+        break
 
 for n in range(height):
-    print(" " * (height - n - 1), end = "")
-    print("#" * (n + 1), end = "")
+    print(" " * (height - n - 1), end="")
+    print("#" * (n + 1), end="")
     print()
